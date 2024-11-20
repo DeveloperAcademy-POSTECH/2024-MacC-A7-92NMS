7d7a8772581f9c4f61cc512226e3c51b85243e9f
VronzeWook <chanel0222@naver.com>
2024-11-20 16:29:28 +0900

    Chore:#33-디자인시스템 재적용

---
+++ Spha/Shared/Extensions/Font+.swift
@@ -0,0 +1,40 @@
+//
+//  Text+.swift
+//  Spha
+//
+//  Created by 최하늘 on 11/20/24.
+//
+
+import SwiftUI
+
+struct CustomFont {
+    let font: Font
+    let kerning: CGFloat
+    let lineSpacing: CGFloat
+    
+    // title (0, 1)
+    static let title_0 = CustomFont(font: .custom("AppleSDGothicNeoSB", size: 32), kerning: 0.4, lineSpacing: 32.0)
+    static let title_1 = CustomFont(font: .custom("AppleSDGothicNeoSB", size: 30), kerning: 0.4, lineSpacing: 30.0)
+    
+    // body (0, 1)
+    static let body_0 = CustomFont(font: .custom("AppleSDGothicNeoSB", size: 20), kerning: 0, lineSpacing: 20.0)
+    static let body_1 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 18), kerning: 0, lineSpacing: 18.0)
+    
+    // caption (0, 1, 2, 3, 4, 5)
+    static let caption_0 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 16), kerning: 0.4, lineSpacing: 16.0)
+    static let caption_1 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 14), kerning: -0.08, lineSpacing: 14.0)
+    static let caption_2 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 12), kerning: -0.08, lineSpacing: 12.0)
+    static let caption_3 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 8), kerning: -0.08, lineSpacing: 8.0)
+    static let caption_4 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 6), kerning: -0.08, lineSpacing: 6.0)
+    static let caption_5 = CustomFont(font: .custom("AppleSDGothicNeoM", size: 5), kerning: -0.08, lineSpacing: 5.0)
+    
+}
+
+extension Text {
+    func customFont(_ customFont: CustomFont) -> some View {
+        self
+            .font(customFont.font)
+            .kerning(customFont.kerning)
+            .lineSpacing(customFont.lineSpacing)
+    }
+}
