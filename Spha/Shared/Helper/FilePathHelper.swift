16ec03d00dbc55ea4d332831d6880dbdac5c3c80
VronzeWook <chanel0222@naver.com>
2024-11-20 21:16:30 +0900

    Chore: #26-FilePathHelper 구현

---
+++ Spha/Shared/Helper/FilePathHelper.swift
@@ -0,0 +1,23 @@
+//
+//  FilePathHelper.swift
+//  Spha
+//
+//  Created by LDW on 11/20/24.
+//
+
+import Foundation
+
+class FilePathHelper {
+    
+    private init() {}
+    
+    // MARK: - File Name
+    static let dirty01 = "dirty01.mp4"
+    static let dirty02 = "dirty02.mp4"
+    static let dirty03 = "dirty03.mp4"
+    static let dirty04 = "dirty04.mp4"
+    
+    static let clean = "clean.mp4"
+    
+    static let none = "none.mp4"
+}
