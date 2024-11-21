40bf125a9df5232b70f6ac273ba4fe257ce3ed30
jiyoungsophia <sophiathedev@gmail.com>
2024-11-21 17:23:54 +0900

    Chore: #41- 스트레스 관련 모델 수정
    - 오늘이 아닌 특정한 하루에 대한 데이터 모델이어서 today가 적합하지 않다고 판단되어 TodayStress -> DailyStressRecord로 구조체명 변경
    - StressLevel 구조체에 hrv 값에 따라 스트레스 레벨 판단하는 getLevel 함수 추가

--- Spha/Shared/Models/StressLevel.swift
+++ Spha/Shared/Models/StressLevel.swift
@@ -15,14 +15,14 @@ enum StressLevel: String, CaseIterable {
     
     var title: String {
         switch self {
-        case .low:
-            "과부하"
-        case .medium:
-            "주의필요"
-        case .high:
-            "정상"
-        case .extreme:
+        case .low:    // hrv >= 55
             "훌륭함"
+        case .medium: // 45< hrv <55
+            "정상"
+        case .high:   //30< hrv <44
+            "주의필요"
+        case .extreme: // hrv<=30
+            "과부하"
         }
     }
     
@@ -32,6 +32,19 @@ enum StressLevel: String, CaseIterable {
         case .medium: return 2
         case .high: return 1
         case .extreme: return 0
+        }
+    }
+    
+    static func getLevel(from hrvValue: Double) -> StressLevel {
+        switch hrvValue {
+        case 55...:
+            return .low       // 훌륭함
+        case 45..<55:
+            return .medium    // 정상
+        case 30..<45:
+            return .high      // 주의필요
+        default: // hrvValue <= 30
+            return .extreme   // 과부하
         }
     }
 }
