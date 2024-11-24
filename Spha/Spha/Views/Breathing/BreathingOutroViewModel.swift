53d733aa5298dab9c3d24726348c59977eb3e4f9
SeoyeonChoo <sy87.business@gmail.com>
2024-11-24 23:42:26 +0900

    Feat: #56-MindfulSession 저장 weak self 수정 반영

--- Spha/Spha/Views/Breathing/BreathingOutroViewModel.swift
+++ Spha/Spha/Views/Breathing/BreathingOutroViewModel.swift
@@ -14,12 +14,15 @@ class BreathingOutroViewModel: ObservableObject {
     private let mindfulSessionManager = MindfulSessionManager()
 
     func fadeOutAnimation(completion: @escaping () -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let self = self else { return }
+
             withAnimation(.easeOut(duration: 1.0)) {
                 self.opacity = 0.0
             }
 
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+                guard let _ = self else { return }
                 completion()
             }
         }
@@ -27,10 +30,11 @@ class BreathingOutroViewModel: ObservableObject {
 
     func recordTestMindfulSession() {
         let startDate = Date()
-        let endDate = Calendar.current.date(byAdding: .minute, value: 10, to: startDate)!
+        let endDate = Calendar.current.date(byAdding: .minute, value: 1, to: startDate)!
 
-        mindfulSessionManager.recordMindfulSession(startDate: startDate, endDate: endDate) { success, error in
+        mindfulSessionManager.recordMindfulSession(startDate: startDate, endDate: endDate) { [weak self] success, error in
             DispatchQueue.main.async {
+                guard let self = self else { return } // self가 해제되었는지 확인
                 if success {
                     self.statusMessage = "Test Mindful Session Recorded Successfully!"
                 } else if let error = error {
@@ -41,4 +45,5 @@ class BreathingOutroViewModel: ObservableObject {
             }
         }
     }
+
 }
