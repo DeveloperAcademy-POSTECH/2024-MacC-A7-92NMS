508821025d7649777db453e580bdf13d0e6d530a
VronzeWook <chanel0222@naver.com>
2024-11-27 01:57:23 +0900

    Feat: #85-폰트적용pbx+Notification오류수정

--- Spha/Shared/Managers/HealthKitManager/HealthKitManager.swift
+++ Spha/Shared/Managers/HealthKitManager/HealthKitManager.swift
@@ -14,31 +14,31 @@ protocol HealthKitInterface {
     func fetchDailyHRV(for date: Date, completion: @escaping ([HKQuantitySample]?, Error?) -> Void) // 일간 HRV 데이터 요청
     func fetchMonthlyHRV(for month: Date, completion: @escaping ([HKQuantitySample]?, Error?) -> Void) // 월간 HRV 데이터 요청
     func monitorHRVUpdates() // HRV 업데이트 모니터링
-    func didUpdateHRVData() // HRV 업데이트 관찰 시 실행 로직
+    func didUpdateHRVData()
 }
 
 class HealthKitManager: HealthKitInterface {
-
-    let healthStore = HKHealthStore()
-    let notificationManager = NotificationManager.shared
     
+    static let shared = HealthKitManager() // 백그라운드 참조 해제 방지용 싱글톤 객체(임시 방편)
+    let healthStore = HKHealthStore()
+ 
     // 권한 요청 메소드
     func requestAuthorization(completion: @escaping (Bool, Error?) -> Void) {
         guard HKHealthStore.isHealthDataAvailable() else {
             completion(false, NSError(domain: "HealthKit", code: 1, userInfo: [NSLocalizedDescriptionKey: "Health data is not available on this device."]))
             return
         }
-
+        
         // 읽기 및 쓰기 권한이 필요한 데이터 타입
         guard let hrvType = HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN),
               let mindfulnessType = HKObjectType.categoryType(forIdentifier: .mindfulSession) else {
             completion(false, NSError(domain: "HealthKit", code: 1, userInfo: [NSLocalizedDescriptionKey: "Unable to create HRV or Mindfulness data types."]))
             return
         }
-
+        
         let readTypes: Set<HKObjectType> = [hrvType, mindfulnessType]
         let shareTypes: Set<HKSampleType> = [hrvType, mindfulnessType]
-
+        
         // 권한 요청
         healthStore.requestAuthorization(toShare: shareTypes, read: readTypes) { success, error in
             if let error = error {
@@ -50,6 +50,7 @@ class HealthKitManager: HealthKitInterface {
                 self.enableBackgroundHRVDelivery()
                 // HRV 데이터 변경 관찰 시작
                 self.monitorHRVUpdates()
+                
                 completion(success, nil)
             }
         }
@@ -62,7 +63,11 @@ class HealthKitManager: HealthKitInterface {
             if let error = error {
                 print("Error enabling background delivery: \(error.localizedDescription)")
             } else {
-                print("HRV 데이터 백그라운드 전달이 활성화되었습니다.")
+                if success {
+                    print("HRV 데이터 백그라운드 전달이 활성화되었습니다.")
+                } else {
+                    print("HRV 데이터 백그라운드 전달이 실패했습니다.")
+                }
             }
         }
     }
@@ -96,29 +101,29 @@ extension HealthKitManager {
     }
     
     // 특정 달의 HRV 데이터 요청
-     func fetchMonthlyHRV(for month: Date, completion: @escaping ([HKQuantitySample]?, Error?) -> Void) {
-         guard let hrvType = HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else {
-             completion(nil, nil)
-             return
-         }
-         
-         let calendar = Calendar.current
-         let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: month))!
-         let endOfMonth = calendar.date(byAdding: .month, value: 1, to: startOfMonth)!
-         
-         let predicate = HKQuery.predicateForSamples(withStart: startOfMonth, end: endOfMonth, options: .strictStartDate)
-         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
-         
-         let query = HKSampleQuery(sampleType: hrvType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { _, results, error in
-             if let samples = results as? [HKQuantitySample] {
-                 completion(samples, nil)
-             } else {
-                 completion(nil, error)
-             }
-         }
-         
-         healthStore.execute(query)
-     }
+    func fetchMonthlyHRV(for month: Date, completion: @escaping ([HKQuantitySample]?, Error?) -> Void) {
+        guard let hrvType = HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else {
+            completion(nil, nil)
+            return
+        }
+        
+        let calendar = Calendar.current
+        let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: month))!
+        let endOfMonth = calendar.date(byAdding: .month, value: 1, to: startOfMonth)!
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startOfMonth, end: endOfMonth, options: .strictStartDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        
+        let query = HKSampleQuery(sampleType: hrvType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { _, results, error in
+            if let samples = results as? [HKQuantitySample] {
+                completion(samples, nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+        
+        healthStore.execute(query)
+    }
     
     // 가장 최근 HRV 데이터를 가져오는 메서드
     func fetchLatestHRV(completion: @escaping (HKQuantitySample?, Error?) -> Void) {
@@ -138,7 +143,7 @@ extension HealthKitManager {
                 completion(nil, error)
             }
         }
-    
+        
         healthStore.execute(query)
     }
 }
@@ -149,7 +154,7 @@ extension HealthKitManager {
     func monitorHRVUpdates() {
         guard let hrvType = HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else { return }
 
-        let query = HKObserverQuery(sampleType: hrvType, predicate: nil) { [weak self] _, completionHandler, error in
+        let query = HKObserverQuery(sampleType: hrvType, predicate: nil) {  _, completionHandler, error in
             if let error = error {
                 print("Error observing HRV updates: \(error.localizedDescription)")
                 completionHandler()
@@ -157,7 +162,8 @@ extension HealthKitManager {
             }
             
             // HRV 데이터 업데이트 시 알림 전송
-            self?.didUpdateHRVData()
+            // 강한 참조로 싱글톤을 사용
+            HealthKitManager.shared.didUpdateHRVData()
             completionHandler()
         }
         
@@ -165,6 +171,20 @@ extension HealthKitManager {
     }
     
     func didUpdateHRVData() {
-        notificationManager.sendBreathingAlert()
+        // 알림 전송 설정
+        let content = UNMutableNotificationContent()
+        content.title = "Spha"
+        content.body = "마음에 먼지가 쌓였어요\n청소하러 가요"
+        content.sound = .default
+        content.categoryIdentifier = "HRV_ALERT"  // 카테고리 지정
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error sending notification: \(error.localizedDescription)")
+            }
+        }
     }
+    
 }
