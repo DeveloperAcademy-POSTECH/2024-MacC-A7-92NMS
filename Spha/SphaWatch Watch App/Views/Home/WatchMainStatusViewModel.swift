f02c1860f20a70e03ae4876fc971b1b3485ba373
jiyoungsophia <sophiathedev@gmail.com>
2024-11-27 20:41:58 +0900

    Chore: #87-WatchMainView 마음구슬 로직 수정

--- Spha/SphaWatch Watch App/Views/Home/WatchMainStatusViewModel.swift
+++ Spha/SphaWatch Watch App/Views/Home/WatchMainStatusViewModel.swift
@@ -7,74 +7,81 @@
 
 import Foundation
 import HealthKit
+import SwiftUI
 
 class WatchMainStatusViewModel: ObservableObject {
-    @Published var showBreathingIntro = false
-    @Published var breathingIntroOpacity: Double = 0.0
-    @Published var recommendedCleaningCount: Int = 0
-    @Published var actualCleaningCount: Int = 0
-
-    // 계산 속성: 남은 청소 횟수
-    var remainingCleaningCount: MindDustLevel {
-        let count = recommendedCleaningCount - actualCleaningCount
-        
-        if count <= 0 { return .dustLevel1}
-        
-        switch count {
-        case 1:
-            return .dustLevel2
-        case 2:
-            return .dustLevel3
-        case 3:
-            return .dustLevel4
-        default:
-            return .dustLevel5
-        }
+    @Published var dailyRecord: DailyStressRecord?
+    
+    var recommendedCount: Int {
+        return dailyRecord?.recommendedReliefCount ?? 0
     }
     
-    // 이용하는 서비스들
-    let HRVService = HealthKitManager()
-    let MindfulService = MindfulSessionManager()
-
-    let stressSdnnValue: Double = 40
+    var completedCount: Int {
+        return dailyRecord?.completedReliefCount ?? 0
+    }
+    
+    var mindDustLevel: String {
+        return dailyRecord?.mindDustLevel.assetName ?? MindDustLevel.none.assetName
+    }
+    
+    var mindDustLvDescription: String {
+        return dailyRecord?.mindDustLevel.description ?? MindDustLevel.none.description
+    }
+    
+    private let healthKitManager: HealthKitInterface
+    private let mindfulSessionManager: MindfulSessionInterface
+    
+    init(_ healthKitManager: HealthKitInterface, _ mindfulSessionManager: MindfulSessionInterface) {
+        self.healthKitManager = HealthKitManager()
+        self.mindfulSessionManager = MindfulSessionManager()
+    }
     
     // DailyHRVData 요청
-    func fetchTodayHRVData() {
-        HRVService.fetchDailyHRV(for: Date()) { samples, error in
-            if let error = error {
-                print("Error fetching daily HRV data: \(error.localizedDescription)")
-            } else if let samples = samples {
-                
-                var stressCount: Int = 0
-                for sample in samples {
-                    let sdnnValue = sample.quantity.doubleValue(for: HKUnit.secondUnit(with: .milli))
-                    if sdnnValue > self.stressSdnnValue {
-                        stressCount += 1
-                    }
-                }
-                DispatchQueue.main.async {
-                    self.recommendedCleaningCount = stressCount
-                }
-                print("Successfully fetched today's HRV data.")
-            } else {
-                DispatchQueue.main.async {
-                    self.recommendedCleaningCount = 0
-                }
-                print("No daily HRV data available.")
+    func updateTodayRecord() {
+        /// HRV 데이터 가져오기
+        healthKitManager.fetchDailyHRV(for: Date()) { [weak self] samples, hrvError in
+            guard let self = self else { return }
+            
+            if let hrvError = hrvError {
+                print("Error fetching daily HRV data: \(hrvError.localizedDescription)")
+                return
             }
-        }
-    }
-    // DailyMindfulSession 요청
-    func fetchTodaySessions() {
-        MindfulService.fetchMindfulSessions(for: Date()) { sessions, error in
-                if let error = error {
-                    print("Error fetching today's sessions: \(error.localizedDescription)")
-                } else if let sessions = sessions {
-                    self.actualCleaningCount = sessions.count
-                } else {
-                    self.actualCleaningCount = 0
-                    print("No sessions found for today.")
+            
+            // TODO: DispatchGroup 고려
+            guard let samples = samples else {
+                print("HRV 데이터 없음")
+                DispatchQueue.main.async {
+                    self.dailyRecord = DailyStressRecord(
+                        date: Date(),
+                        recommendedReliefCount: 0,
+                        completedReliefCount: 0
+                    )
                 }
+                return
+            }
+            
+            /// 마음챙기기 데이터 가져오기
+            self.mindfulSessionManager.fetchMindfulSessions(for: Date()) { sessions, mindError in
+                if let mindError = mindError {
+                    print("Error fetching daily mindSession data: \(mindError.localizedDescription)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    /// 일일 마음 청소 통계 업데이트
+                    let highStressSamples = samples.filter { sample in
+                        let hrvValue = sample.quantity.doubleValue(for: HKUnit.secondUnit(with: .milli))
+                        let stressLevel = StressLevel.getLevel(from: hrvValue)
+                        return stressLevel == .high || stressLevel == .extreme
+                    }
+                    
+                    self.dailyRecord = DailyStressRecord(
+                        date: Date(),
+                        recommendedReliefCount: highStressSamples.count,
+                        completedReliefCount: sessions?.count ?? 0
+                    )
+                }
+            }
         }
     }
 
