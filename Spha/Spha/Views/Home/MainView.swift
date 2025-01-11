476aaff01293be3df65e0f17c61539234440e4c3
SeoyeonChoo <sy87.business@gmail.com>
2025-01-11 10:58:27 +0900

    Chore: #135-Localization 추가 작업

--- Spha/Spha/Views/Home/MainView.swift
+++ Spha/Spha/Views/Home/MainView.swift
@@ -58,6 +58,7 @@ struct MainView: View {
                     )
                     .bold()
                     .padding(.top, 8)
+                    .multilineTextAlignment(.center)
                 
                 Spacer()
                 
